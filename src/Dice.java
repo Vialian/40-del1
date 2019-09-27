95422354945dca214cb0ce15a43a5bec835b3660
Vialian <dotasoren@gmail.com>
2019-09-27 12:13:55 +0200

    Dice med 1000 kast test

--- src/Dice.java
+++ src/Dice.java
@@ -16,15 +16,17 @@ public class Dice {
     private static void TestOfDice()
     {
 
-        double tal1 = 0,tal2 = 0,tal3 = 0,tal4 = 0,tal5 = 0,tal6 = 0, totalAmount = 0;
-
+        double tal2 = 0,tal3 = 0,tal4 = 0,tal5 = 0,tal6 = 0, tal7 = 0, tal8 = 0, tal9 = 0, tal10 = 0, tal11 = 0, tal12 = 0, totalAmount = 0;
+int diceDouble = 0;
         while(totalAmount < 1000)
         {
-            Random r = new Random();
-            double tal = r.nextInt((6 - 1) + 1) + 1;
-            if (tal == 1)
-                tal1++;
-            else if (tal == 2)
+            Random rand1 = new Random();
+
+            Random rand2 = new Random();
+            double temptal1 = rand1.nextInt((6 - 1) + 1) + 1;
+            double temptal2 = rand2.nextInt((6 - 1) + 1) + 1;
+            double tal = temptal1 + temptal2;
+            if (tal == 2)
                 tal2++;
             else if (tal == 3)
                 tal3++;
@@ -34,19 +36,42 @@ public class Dice {
                 tal5++;
             else if (tal == 6)
                 tal6++;
+            else if (tal == 7)
+                tal7++;
+            else if (tal == 8)
+                tal8++;
+            else if (tal == 9)
+                tal9++;
+            else if (tal == 10)
+                tal10++;
+            else if (tal == 11)
+                tal11++;
+            else if (tal == 12)
+                tal12++;
             totalAmount++;
+            if(temptal1 == temptal2)
+                diceDouble++;
+
         }
-        System.out.println("1: " + tal1 + " 2: " + tal2 + " 3: " + tal3 + " 4:" + tal4 + " 5: " + tal5 + " 6: " + tal6);
+        System.out.println("2: " + tal2 + " 3: " + tal3 + " 4:" + tal4 + " 5: " + tal5 + " 6: " + tal6 +
+                "7: " + tal7 + " 8: " + tal8 + " 9:" + tal9 + " 10: " + tal10 + " 11: " + tal11 + " 12: " + tal12);
 
-
-        double procentTal1 = (tal1/1000)*100;
         double procentTal2 = (tal2/1000)*100;
         double procentTal3 = (tal3/1000)*100;
         double procentTal4 = (tal4/1000)*100;
         double procentTal5 = (tal5/1000)*100;
         double procentTal6 = (tal6/1000)*100;
+        double procentTal7 = (tal7/1000)*100;
+        double procentTal8 = (tal8/1000)*100;
+        double procentTal9 = (tal9/1000)*100;
+        double procentTal10 = (tal10/1000)*100;
+        double procentTal11= (tal11/1000)*100;
+        double procentTal12 = (tal12/1000)*100;
 
-        System.out.println("1: " + procentTal1 + "% 2: " + procentTal2 + "% 3: " + procentTal3 + "% 4:" + procentTal4 +
-                "% 5: " + procentTal5 + "% 6: " + procentTal6 + "%");
+        System.out.println("2: " + procentTal2 + "% 3: " + procentTal3 + "% 4:" + procentTal4 +
+                "% 5: " + procentTal5 + "% 6: " + procentTal6 + "% 7: " + procentTal7 + "% 8: "
+                + procentTal8 + "% 9: " + procentTal9 + "% 10: "
+                + procentTal10 + "% 11: " + procentTal11 + "% 12: " + procentTal12 + "%");
+        System.out.println("double throw: " + diceDouble);
     }
 }
