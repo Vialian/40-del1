cf088f37133995e5c2e5432ac92c8eb32ecb75b6
Vialian <dotasoren@gmail.com>
2019-09-27 15:31:34 +0200

    Dice med 1000 kast, med en lille fejl med total %

--- src/Dice.java
+++ src/Dice.java
@@ -1,4 +1,4 @@
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Dice {
@@ -15,63 +15,51 @@ public class Dice {
     }
     private static void TestOfDice()
     {
+        Map<Integer, List<Integer>> dice = new HashMap<>();
 
-        double tal2 = 0,tal3 = 0,tal4 = 0,tal5 = 0,tal6 = 0, tal7 = 0, tal8 = 0, tal9 = 0, tal10 = 0, tal11 = 0, tal12 = 0, totalAmount = 0;
-int diceDouble = 0;
-        while(totalAmount < 1000)
+        for (int i = 2; i <= 12;)
+        {
+            dice.put(i, new ArrayList<>());
+            i++;
+        }
+
+        int totalAmount = 0, diceDouble = 0, maxAmount = 1000;
+        while(totalAmount < maxAmount)
         {
             Random rand1 = new Random();
-
             Random rand2 = new Random();
-            double temptal1 = rand1.nextInt((6 - 1) + 1) + 1;
-            double temptal2 = rand2.nextInt((6 - 1) + 1) + 1;
-            double tal = temptal1 + temptal2;
-            if (tal == 2)
-                tal2++;
-            else if (tal == 3)
-                tal3++;
-            else if (tal == 4)
-                tal4++;
-            else if (tal == 5)
-                tal5++;
-            else if (tal == 6)
-                tal6++;
-            else if (tal == 7)
-                tal7++;
-            else if (tal == 8)
-                tal8++;
-            else if (tal == 9)
-                tal9++;
-            else if (tal == 10)
-                tal10++;
-            else if (tal == 11)
-                tal11++;
-            else if (tal == 12)
-                tal12++;
-            totalAmount++;
+
+            int temptal1 = rand1.nextInt((6 - 1) + 1) + 1;
+            int temptal2 = rand2.nextInt((6 - 1) + 1) + 1;
+
+
+            int tal = temptal1 + temptal2;
+            dice.get(tal).add(1);
             if(temptal1 == temptal2)
                 diceDouble++;
 
+            totalAmount++;
         }
-        System.out.println("2: " + tal2 + " 3: " + tal3 + " 4:" + tal4 + " 5: " + tal5 + " 6: " + tal6 +
-                "7: " + tal7 + " 8: " + tal8 + " 9:" + tal9 + " 10: " + tal10 + " 11: " + tal11 + " 12: " + tal12);
 
-        double procentTal2 = (tal2/1000)*100;
-        double procentTal3 = (tal3/1000)*100;
-        double procentTal4 = (tal4/1000)*100;
-        double procentTal5 = (tal5/1000)*100;
-        double procentTal6 = (tal6/1000)*100;
-        double procentTal7 = (tal7/1000)*100;
-        double procentTal8 = (tal8/1000)*100;
-        double procentTal9 = (tal9/1000)*100;
-        double procentTal10 = (tal10/1000)*100;
-        double procentTal11= (tal11/1000)*100;
-        double procentTal12 = (tal12/1000)*100;
+        for (int i = 2; i <= 12;)
+        {
+            System.out.println(i + ": " + dice.get(i).size());
+            i++;
 
-        System.out.println("2: " + procentTal2 + "% 3: " + procentTal3 + "% 4:" + procentTal4 +
-                "% 5: " + procentTal5 + "% 6: " + procentTal6 + "% 7: " + procentTal7 + "% 8: "
-                + procentTal8 + "% 9: " + procentTal9 + "% 10: "
-                + procentTal10 + "% 11: " + procentTal11 + "% 12: " + procentTal12 + "%");
-        System.out.println("double throw: " + diceDouble);
+        }
+
+        System.out.println("I alt blev der slået: " + diceDouble + " ens");
+
+        int test = 0;
+        for (int i = 2; i <= 12;)
+        {
+            double temp = (dice.get(i).size()*100)/1000;
+
+            test += temp;
+            System.out.println(i + ": " + temp + "%");
+            i++;
+        }
+
+        System.out.println("total: "+test+"%");
     }
 }
