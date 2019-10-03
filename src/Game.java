a7aa8522a1c625d087f04fab701527123b79fcca
Alexander Lambrecht <alexanderlambrecht@gmail.com>
2019-10-03 22:43:13 +0200

    Fikset så både player 1 og player 2 kan vinde, man kna dog ikke vinde ved 2 gange 6 og 6

--- src/Game.java
+++ src/Game.java
@@ -1,71 +1,74 @@
 public class Game {
 
-    private int point1; private int point2 ;
+    private static int point1; private static int point2 ;
 
     //getterasfasf
-    public int getPoint1() {
-        System.out.println("get point =" + point1);
+    public static int getPoint1() {
+       // System.out.println("get point 1 =" + point1);
         return point1;
 
     }
     //setter
-    public void setPoint(int x1) {
+    public static void setPoint1(int x1) {
         point1 = x1;
-        System.out.println(point1);
+       // System.out.println("point1 sat til "+point1);
     }
     //getter
-    public int getPoint2() {
-        System.out.println("get point =" + point2);
+    public static int getPoint2() {
+        //System.out.println("get point 2 =" + point2);
         return point2;
 
     }
     //setter
-    public void setPoint2(int x2) {
+    public static void setPoint2(int x2) {
         point2 = x2;
-        System.out.println(point2);
+       // System.out.println("point1 sat til "+ point2);
     }
-
+    public static boolean double6 = false;
 
     public static void main(String[] args){
-        Game game = new Game();
+
         boolean player = false;
         while(true){
             boolean won1 = checknAdd(roll(), roll(), player);
-            System.out.println(game.getPoint1());
+            //System.out.println(getPoint1() + " | " + getPoint2() );
             player = !player;
+            double6 = false;
             boolean won2 = checknAdd(roll(), roll(), player);
-            System.out.println(game.getPoint2());
+            //System.out.println(getPoint2());
             player = !player;
+            double6 = false;
+
             if(won1 || won2){
                 if(won1 & won2){
-                    if(game.getPoint1()>game.getPoint2()){
-                        game.Player1Wins();
+                    if(getPoint1()>getPoint2()){
+                        Player1Wins();
                         break;
                     }
-                    if(game.getPoint1()<game.getPoint2()){
-                        game.Player2Wins();
+                    if(getPoint1()<getPoint2()){
+                        Player2Wins();
                         break;
                     }
-                    game.Tie();
+                    Tie();
                     break;
                 }
                 if(won1){
-                    game.Player1Wins();
+                    Player1Wins();
                     break;
                 }
-                game.Player2Wins();
+                Player2Wins();
                 break;
             }
         }
     }
-        public void Player1Wins(){
+        public static void Player1Wins(){
             System.out.println("spiller 1 har vundet");
 
         }
-        public void Player2Wins(){
+        public static void Player2Wins(){
         System.out.println("spiller 2 har vundet");
         }
-        public void Tie(){
+        public static void Tie(){
             System.out.println("Lige");
          }
 
@@ -76,25 +79,30 @@ public class Game {
         }
 
         public static boolean checknAdd(int r1, int r2, boolean player) {
-            Game game = new Game();
+
           //  System.out.println("roll 1 =" + r1 +" _ roll 2 = "+r2);
-            boolean double6 = false; // boolean der viser om der er blevet slået 6,6 slaget før // fiks lige pls
+            // boolean der viser om der er blevet slået 6,6 slaget før // fiks lige pls
             //add point først
             if (player == true) { //hvis det er spiller 1
-
-                game.setPoint(game.getPoint1() + r1 + r2);
+               // System.out.println("p1 er kørt");
+                setPoint1((getPoint1() + r1 + r2));
                 if (r1 == r2) {
-                    //checker om der er blevet slået par
+                    if (getPoint1() >= 40) {
+
+                        System.out.println("player 1 "+r1 + " " + r2);
+                        return true;
+                    }
                     if (r1 == 1) { //hvis der er blevet slået 1,1 sættes spillerns point til 1
                        // System.out.println("1,1 er kørt");
-                        game.setPoint(0);
-                        double6 = false;
+                        setPoint1(0);
+
                         return false;
                     }
                     if (r1 == 6) { // Hvis der er blevet slået 6,6
                         //System.out.println("6.6 kørt");
                         if (double6 == true) {
-                            double6 = false;
+
+                            System.out.println("player 1 vundet med 66 __" + r1 + " " + r2);
                             return true;
                         } else {
                             double6 = true;
@@ -104,27 +112,28 @@ public class Game {
                 }
             }
             if (player == false) { //player 2, samme som player1
-
-                game.setPoint(game.getPoint2() + r1 + r2);
+                //System.out.println("p2 er kørt");
+                setPoint2(getPoint2() + r1 + r2);
 
                 if (r1 == r2) {
-                    if (game.getPoint2() >= 40) {
-                    double6 = false;
+                    if (getPoint2() >= 40) {
+                    System.out.println("player 2 "+r1 + " " + r2);
                     return true;
                 }
 
                     if (r1 == 1) {
-                        game.setPoint2(0);
-                        double6 = false;
+                        setPoint2(0);
+
                         return false;
                     }
                     if (r1 == 6) {
                         if (double6 == true) {
-                            double6 = false;
+
+                            System.out.println("player 2 vundet med 66 __ "+r1 + " " + r2);
                             return true;
                         } else {
                             double6 = true;
-                            return checknAdd(roll(), roll(), true);
+                            return checknAdd(roll(), roll(), false);
                         }
                     }
                 }
