3613d0883410984eef1098a861f53ee8aaca620a
Alexander Lambrecht <alexanderlambrecht@gmail.com>
2019-10-04 11:17:35 +0200

    Fikset så både player 1 og player 2 kan vinde, man kna dog ikke vinde ved 2 gange 6 og 6

--- src/Game.java
+++ src/Game.java
@@ -24,7 +24,7 @@ public class Game {
         point2 = x2;
        // System.out.println("point1 sat til "+ point2);
     }
-    public static boolean double6 = false;
+    public static boolean rolled6 = false;
 
     public static void main(String[] args){
 
@@ -33,11 +33,11 @@ public class Game {
             boolean won1 = checknAdd(roll(), roll(), player);
             //System.out.println(getPoint1() + " | " + getPoint2() );
             player = !player;
-            double6 = false;
-            boolean won2 = checknAdd(roll(), roll(), player);
+
+            //boolean won2 = checknAdd(roll(), roll(), player);
+            boolean won2 = checknAdd(roll(),roll(), player);
             //System.out.println(getPoint2());
             player = !player;
-            double6 = false;
 
             if(won1 || won2){
                 if(won1 & won2){
@@ -80,65 +80,74 @@ public class Game {
 
         public static boolean checknAdd(int r1, int r2, boolean player) {
 
-          //  System.out.println("roll 1 =" + r1 +" _ roll 2 = "+r2);
+          System.out.println( r1 +" og "+r2 + player + "points: ");
             // boolean der viser om der er blevet slået 6,6 slaget før // fiks lige pls
             //add point først
-            if (player == true) { //hvis det er spiller 1
-               // System.out.println("p1 er kørt");
+            if (player == false) { //hvis det er spiller 1
+
                 setPoint1((getPoint1() + r1 + r2));
                 if (r1 == r2) {
-                    if (getPoint1() >= 40) {
-
+                    if (getPoint1() >= 40 + r1 + r2) {
                         System.out.println("player 1 "+r1 + " " + r2);
+                        checknAdd(roll(), roll(), false);
+                        if (r1 == 1) { //hvis der er blevet slået 1,1 sættes spillerns point til 1
+
+                            setPoint1(0);
+                        }
                         return true;
                     }
                     if (r1 == 1) { //hvis der er blevet slået 1,1 sættes spillerns point til 1
-                       // System.out.println("1,1 er kørt");
+                        System.out.println("1,1 er kørt");
                         setPoint1(0);
 
-                        return false;
                     }
                     if (r1 == 6) { // Hvis der er blevet slået 6,6
-                        //System.out.println("6.6 kørt");
-                        if (double6 == true) {
 
+                        if (rolled6) {
                             System.out.println("player 1 vundet med 66 __" + r1 + " " + r2);
+                            checknAdd(roll(), roll(), false);
                             return true;
-                        } else {
-                            double6 = true;
-                            return checknAdd(roll(), roll(), true); //hvis der er blevet slået 6,6 får spillern et ekstra slag
                         }
+                        rolled6 = true;
+                        return checknAdd(roll(), roll(), false); //hvis der er blevet slået 6,6 får spillern et ekstra slag
                     }
+                    rolled6 = false;
+                    return checknAdd(roll(), roll(), false);
                 }
             }
-            if (player == false) { //player 2, samme som player1
-                //System.out.println("p2 er kørt");
-                setPoint2(getPoint2() + r1 + r2);
-
+            if (player == true) { //hvis det er spiller 1
+                // System.out.println("p1 er kørt");
+                setPoint2((getPoint2() + r1 + r2));
                 if (r1 == r2) {
-                    if (getPoint2() >= 40) {
-                    System.out.println("player 2 "+r1 + " " + r2);
-                    return true;
-                }
+                    if (getPoint2() >= 40 + r1 + r2) {
 
-                    if (r1 == 1) {
+                        checknAdd(roll(), roll(), true);
+                        if (r1 == 1) { //hvis der er blevet slået 1,1 sættes spillerns point til 1
+                            System.out.println("1,1 er kørt");
+                            setPoint2(0);
+                        }
+                        return true;
+                    }
+                    if (r1 == 1) { //hvis der er blevet slået 1,1 sættes spillerns point til 1
+                        System.out.println("1,1 er kørt");
                         setPoint2(0);
 
-                        return false;
                     }
-                    if (r1 == 6) {
-                        if (double6 == true) {
+                    if (r1 == 6) { // Hvis der er blevet slået 6,6
 
-                            System.out.println("player 2 vundet med 66 __ "+r1 + " " + r2);
+                        if (rolled6) {
+
+                            checknAdd(roll(), roll(), true);
                             return true;
-                        } else {
-                            double6 = true;
-                            return checknAdd(roll(), roll(), false);
                         }
+                        rolled6 = true;
+                        return checknAdd(roll(), roll(), true); //hvis der er blevet slået 6,6 får spillern et ekstra slag
                     }
+                    rolled6 = false;
+                    return checknAdd(roll(), roll(), true);
                 }
             }
-            double6 = false; //hvis der ikke er blevet slået par 6,6 eller 1,1, sættes double 6 til false, og der bliver retuneret false
+            rolled6 = false;
             return false;
         }
 
